85e214e2e76184c3edc800099d4dcb1202ae1d89
carrlox <carlito1999@live.com>
2024-05-06 09:07:08 -0500

    conectando las relaciones de la base de datos

--- _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/domain/entity/Appointment.java
+++ _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/domain/entity/Appointment.java
@@ -4,16 +4,19 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity (name = "appointment")
+@Entity(name = "appointment")
 @Data
 @Builder
 @AllArgsConstructor
@@ -22,10 +25,15 @@ public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column (nullable = false)
+    @Column(nullable = false)
     private LocalDateTime dateTime;
-    @Column (nullable = false)
+    @Column(nullable = false)
     private Integer duration;
-    @Lob //mapear a tipo TETX en la BD
+    @Lob // mapear a tipo TETX en la BD
     private String comments;
+
+    // relaciones
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private ClientEntity client;
 }
