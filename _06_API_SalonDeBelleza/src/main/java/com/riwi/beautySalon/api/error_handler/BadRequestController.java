75692528aa13517e5ec2b6e5d539679a643dc82a
carrlox <carlito1999@live.com>
2024-05-08 07:36:36 -0500

    manejo de errores generico part 2

--- _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/api/error_handler/BadRequestController.java
+++ _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/api/error_handler/BadRequestController.java
@@ -11,24 +11,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.riwi.beautySalon.api.dto.errors.BaseErrorResponse;
 import com.riwi.beautySalon.api.dto.errors.ErrorsResp;
+import com.riwi.beautySalon.utils.enums.exceptions.BadRequestException;
 
 @RestControllerAdvice
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class BadRequestController {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public BaseErrorResponse handleBadRequest(
-            MethodArgumentNotValidException exception) {
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public BaseErrorResponse handleBadRequest(
+                        MethodArgumentNotValidException exception) {
 
-        List<String> errors = new ArrayList<>();
+                List<String> errors = new ArrayList<>();
 
-        exception.getAllErrors()
-                .forEach(error -> errors.add(error.getDefaultMessage()));
+                exception.getAllErrors()
+                                .forEach(error -> errors.add(error.getDefaultMessage()));
 
-        return ErrorsResp.builder()
-                .code(HttpStatus.BAD_REQUEST.value())
-                .status(HttpStatus.BAD_REQUEST.name())
-                .errors(errors)
-                .build();
-    }
+                return ErrorsResp.builder()
+                                .code(HttpStatus.BAD_REQUEST.value())
+                                .status(HttpStatus.BAD_REQUEST.name())
+                                .errors(errors)
+                                .build();
+        }
+
+        @ExceptionHandler(BadRequestException.class)
+        public BaseErrorResponse badRequest(BadRequestException exception) {
+                List<String> errors = new ArrayList<>();
+
+                errors.add(exception.getMessage());
+
+                return ErrorsResp.builder()
+                                .code(HttpStatus.BAD_REQUEST.value())
+                                .status(HttpStatus.BAD_REQUEST.name())
+                                .errors(errors)
+                                .build();
+        }
 }
