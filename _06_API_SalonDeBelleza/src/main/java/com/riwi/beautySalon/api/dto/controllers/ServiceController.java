5b5238f26b06f3d0b7e1f9f7a3858019de64af6d
carrlox <carlito1999@live.com>
2024-05-08 08:39:59 -0500

    update

--- _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/api/dto/controllers/ServiceController.java
+++ _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/api/dto/controllers/ServiceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,5 +65,13 @@ public class ServiceController {
         this.service.delete(id);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<ServiceResp>update(
+        @Validated @RequestBody ServiceReq request,
+        @PathVariable Long id
+    ){
+        return ResponseEntity.ok(this.service.update(request, id));
     }
 }
