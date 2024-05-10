945d27cf09208cb303d1a16eb7b8f954e6380451
carrlox <carlito1999@live.com>
2024-05-10 06:33:10 -0500

    :v

--- _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/api/dto/request/EmployeeReq.java
+++ _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/api/dto/request/EmployeeReq.java
@@ -4,6 +4,7 @@ import com.riwi.beautySalon.utils.enums.RoleEmployee;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
