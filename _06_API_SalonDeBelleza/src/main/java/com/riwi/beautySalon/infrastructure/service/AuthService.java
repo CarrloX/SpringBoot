d0729762e63d3d0e494326b4d9794c8211f53be5
carrlox <carlito1999@live.com>
2024-05-10 07:35:18 -0500

    servicio de autentication

--- _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/infrastructure/service/AuthService.java
+++ _06_API_SalonDeBelleza/src/main/java/com/riwi/beautySalon/infrastructure/service/AuthService.java
@@ -1,5 +1,60 @@
 package com.riwi.beautySalon.infrastructure.service;
 
-public class AuthService {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.riwi.beautySalon.api.dto.request.LoginReq;
+import com.riwi.beautySalon.api.dto.request.RegisterReq;
+import com.riwi.beautySalon.api.dto.response.AuthResp;
+import com.riwi.beautySalon.domain.entity.User;
+import com.riwi.beautySalon.domain.repositories.UserRepository;
+import com.riwi.beautySalon.infrastructure.abstract_services.IAuthService;
+import com.riwi.beautySalon.utils.enums.Role;
+import com.riwi.beautySalon.utils.enums.exceptions.BadRequestException;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class AuthService implements IAuthService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthResp login(LoginReq request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+    @Override
+    public AuthResp register(RegisterReq request) {
+        // 1.validar que el username no exista
+        User exist = this.findByUserName(request.getUserName());
+
+        if (exist != null) {
+            throw new BadRequestException("este usuario ya esta registrado");
+        }
+
+        // 2.construimos el nuevo usuario
+        User user = User.builder()
+                .userName(request.getUserName())
+                .password(request.getPassword())
+                .role(Role.CLIENT)
+                .build();
+
+        // 3.guardar el nuevo usuario en la DB
+        this.userRepository.save(user);
+
+        return AuthResp.builder()
+                .message("se registro exitosamente")
+                .token(null)
+                .build();
+    }
+
+    private User findByUserName(String userName) {
+        return this.userRepository.findByUsername(userName)
+                .orElse(null);
+    }
+
 }
